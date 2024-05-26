8c0d730adc1f8a042267ee2af6f4a49e775b5d6b
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-05-26 17:18:10 +0300

    Update main.tf

--- k8s-echo-server/modules/eks/main.tf
+++ k8s-echo-server/modules/eks/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
-   tags = {
+  tags = {
     Name = boaz
     Owner = Nati 
     Department = DevOps
