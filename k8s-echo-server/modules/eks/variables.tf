fff22a048c6a36338e07ac38ee9cfdbec5f6627a
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-05-26 21:36:03 +0300

    Update variables.tf

--- k8s-echo-server/modules/eks/variables.tf
+++ k8s-echo-server/modules/eks/variables.tf
@@ -1,42 +1,8 @@
-variable "ssh_key_name" {
-  description = "ssh_key_name"
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
   type        = string
+  default     = "eu-central-1"
 }
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  type        = string
-}
-variable "vpc_id" {
-  description = "vpc_id"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs"
-  type        = list(string)
-}
-
-variable "desired_capacity" {
-  description = "Desired number of worker nodes"
-  type        = number
-}
-
-variable "max_capacity" {
-  description = "Maximum number of worker nodes"
-  type        = number
-}
-
-variable "min_capacity" {
-  description = "Minimum number of worker nodes"
-  type        = number
-}
-
-variable "instance_type" {
-  description = "EC2 instance type for the nodes"
-  type        = string
-}
-variable "vpc_cidr_block" {
-  description = "vpc_cidr_block"
-  type        = string
-}
-
