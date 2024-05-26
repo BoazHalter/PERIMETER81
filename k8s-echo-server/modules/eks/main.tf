45e01182e9a53f60fe3685c577ab67f40eaa5731
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-05-26 17:35:37 +0300

    Update main.tf

--- k8s-echo-server/modules/eks/main.tf
+++ k8s-echo-server/modules/eks/main.tf
@@ -84,19 +84,3 @@ resource "aws_iam_role_policy_attachment" "eks_registry" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-output "cluster_id" {
-  value = aws_eks_cluster.this.id
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
-
-output "cluster_certificate_authority" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.this.id
-}
-
