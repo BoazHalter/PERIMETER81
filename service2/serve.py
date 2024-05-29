e198fe6bd773a2e425c3b23edb386b539db20568
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-05-30 00:39:48 +0300

    Update serve.py

--- service2/serve.py
+++ service2/serve.py
@@ -22,13 +22,24 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             # Get the environment variable
             environment_value = os.getenv('ENVIRONMENT', 'Environment variable not set')
             server_ip = os.getenv('ECHO_SERVER_ECHO_SERVER_PORT_8080_TCP_ADDR','TCP_ADDR not set')
-            client_ip = self.client_address[0]
-            # Inject the environment variable into the HTML content
+            
+            # Extract client IP from X-Forwarded-For header if present
+            client_ip = self.headers.get('X-Forwarded-For', None)
+            if client_ip:
+                # X-Forwarded-For can contain a comma-separated list of IPs; the client's IP is typically first
+                client_ip = client_ip.split(',')[0].strip()
+
+            # If X-Forwarded-For header is not present or does not contain a valid IP, fallback to self.client_address
+            if not client_ip:
+                client_ip = self.client_address[0]
+                
+            # Inject the environment variable and IP addresses into the HTML content
             modified_html_content = html_content.replace(
                 '</body>',
-                f'<div>Environment: {environment_value}</div></body>'
-                f'<div>Server IP: {server_ip}</div></body>'
-                f'<div>Client IP: {client_ip}</div></body>'
+                f'<div>Environment: {environment_value}</div>'
+                f'<div>Server IP: {server_ip}</div>'
+                f'<div>Client IP: {client_ip}</div>'
+                '</body>'
             )
 
             # Send the modified HTML content
